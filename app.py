01cbfd840e44836744916e670454039f8a9ec866
pratikkhandake <160304215+pratikkhandake@users.noreply.github.com>
2024-02-29 14:49:29 +0530

    Update app.py
    Signed-off-by: pratikkhandake <160304215+pratikkhandake@users.noreply.github.com>

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def prediction():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(port=port_number, debug=False)
+    app.run(host='0.0.0.0',port=port_number, debug=False)
